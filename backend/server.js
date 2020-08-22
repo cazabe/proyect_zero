d906c56bae4d187af43e9ad11951fbb1db69a0aa
cazabe <cazcarlos@hotmail.com>
2020-08-21 20:21:33 -0500

    Adding gitignore and installing nodemon dependency

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+//routes
+
+app.get("/registro");
+
 app.listen(PORT , ()=>{
     console.log(`Listen on port ${PORT}`);
 })
