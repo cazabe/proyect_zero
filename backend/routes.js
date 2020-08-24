3e546874fa128c135f829eba071d5c36b76762fe
bryancalisto@yahoo.es <bryancalisto@yahoo.es>
2020-08-23 22:53:10 -0500

    Added use of express.static and created button classes in styles.css

--- backend/routes.js
+++ backend/routes.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const path = require('path');
 const route = express.Router();
-const loginController = require("./controllers/loginController")
+const loginController = require("./controllers/loginController");
+const userController = require("./controllers/userController");
 
 route.post("/login" , loginController.register);
+
+route.get("/users" , (req, res) => {
+    res.sendFile(path.join(__dirname,'../frontend/users.html'));
+});
 
 module.exports = route;
